55c0f501f742355f741a6f59c3ade02a264cf583
MoskeyOmbus <john.wnek@gmail.com>
2013-10-15 02:24:32 -0400

    adding inline edit box, doesnt work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,4 +8,15 @@ angular.module('angularificationApp')
       'Light',
       'Gravity'
     ];
+   	$scope.showtooltip = false;
+	$scope.value = 'Edit me.';
+	$scope.hideTooltip = function(){
+		console.log('hide');
+		$scope.showtooltip = false;
+	}
+	$scope.toggleTooltip = function(e){
+		console.log('toggle')
+		e.stopPropagation();
+		$scope.showtooltip = !$scope.showtooltip;
+	}
   });
